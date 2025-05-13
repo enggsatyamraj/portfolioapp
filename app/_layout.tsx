f47f0fb9490428c8600b495cb66b5b4c2cbd47e2
Satyam Raj <e22cseu0257@bennett.edu.in>
2025-05-13 15:08:16 +0530

    it is showing man

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ import "../global.css";
 export default function Layout() {
     return (
         <Stack>
-
+            <Stack.Screen name="index" options={{ headerShown: false }} />
         </Stack>
     )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
+import { WebView } from 'react-native-webview';
+import { View, Text, StyleSheet, Dimensions } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function index() {
     return (
-        <View>
-            <Text className='text-2xl font-bold bg-red-800'>Nativewind</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <WebView
+                style={styles.webview}
+                source={{ uri: 'https://enggsatyamraj.vercel.app/' }}
+                javaScriptEnabled={true}
+                domStorageEnabled={true}
+            />
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    webview: {
+        flex: 1,
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    }
+});
