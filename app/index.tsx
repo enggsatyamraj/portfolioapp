4ab4ae8c51d84dd799e1e20617a1219dd18619ff
Satyam Raj <e22cseu0257@bennett.edu.in>
2025-05-13 14:40:47 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function index() {
+    return (
+        <View>
+            <Text className='text-2xl font-bold bg-red-800'>Nativewind</Text>
+        </View>
+    )
+}
